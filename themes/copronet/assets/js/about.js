c5f8efbb31ecf15d1423401929a9bbe070e0e969
gil-x <gino.ladowitch@gmail.com>
2026-01-30 13:21:01 +0100

    💄 style: improved dual button
    Signed-off-by: gil-x <gino.ladowitch@gmail.com>

--- themes/copronet/assets/js/about.js
+++ themes/copronet/assets/js/about.js
@@ -1,12 +1,74 @@
+function checkSide(event) {
+  const rect = event.target.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  // Calcul des distances aux bords
+  const top = y;
+  const bottom = rect.height - y;
+  const left = x;
+  const right = rect.width - x;
+
+  // Le plus petit = côté d'entrée
+  const min = Math.min(top, bottom, left, right);
+
+  let side;
+  if (min === top) side = "top";
+  else if (min === bottom) side = "bottom";
+  else if (min === left) side = "left";
+  else side = "right";
+
+  return side;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const lButton = document.querySelector(".left");
   const rButton = document.querySelector(".right");
 
-  rButton.addEventListener("mouseover", function () {
-    lButton.classList.toggle("hovered");
+  lButton.addEventListener("mouseenter", function (event) {
+    let side = checkSide(event);
+
+    if (side === "right") {
+      lButton.classList.remove("hovered");
+      rButton.classList.remove("hovered");
+    } else {
+      lButton.classList.add("hovered");
+      rButton.classList.add("hovered");
+    }
   });
-  rButton.addEventListener("mouseout", function () {
-    lButton.classList.remove("hovered");
+
+  lButton.addEventListener("mouseleave", function (event) {
+    let side = checkSide(event);
+    if (side === "right") {
+      lButton.classList.add("hovered");
+      rButton.classList.add("hovered");
+    } else {
+      lButton.classList.remove("hovered");
+      rButton.classList.remove("hovered");
+    }
+  });
+
+  rButton.addEventListener("mouseenter", function (event) {
+    let side = checkSide(event);
+
+    if (side === "left") {
+      rButton.classList.remove("hovered");
+      lButton.classList.remove("hovered");
+    } else {
+      rButton.classList.add("hovered");
+      lButton.classList.add("hovered");
+    }
+  });
+
+  rButton.addEventListener("mouseleave", function (event) {
+    let side = checkSide(event);
+    if (side === "right") {
+      rButton.classList.add("hovered");
+      lButton.classList.add("hovered");
+    } else {
+      rButton.classList.remove("hovered");
+      lButton.classList.remove("hovered");
+    }
   });
 });
 
